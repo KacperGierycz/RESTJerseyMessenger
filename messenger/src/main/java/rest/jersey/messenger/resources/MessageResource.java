64903a01951d84db1e6e17cf42f550f42cef6090
Kacper <kacpergierycz@gmail.com>
2021-07-07 13:58:33 +0200

    @PUT

--- messenger/src/main/java/rest/jersey/messenger/resources/MessageResource.java
+++ messenger/src/main/java/rest/jersey/messenger/resources/MessageResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,6 +32,15 @@ public class MessageResource {
 	public Message addMessage(Message message) {
 		return messageService.addMessage(message);
 		 
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId")long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
 	}
 	
 	@GET
