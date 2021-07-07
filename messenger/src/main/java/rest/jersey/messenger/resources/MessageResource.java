7144f21191f20a957a7a98fe6a46546ddc84f86c
Kacper <kacpergierycz@gmail.com>
2021-07-07 13:38:17 +0200

    POM json, POST message

--- messenger/src/main/java/rest/jersey/messenger/resources/MessageResource.java
+++ messenger/src/main/java/rest/jersey/messenger/resources/MessageResource.java
@@ -3,6 +3,7 @@ package rest.jersey.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,15 +18,21 @@ public class MessageResource {
 	MessageService messageService = new MessageService ();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		
 		return messageService.getAllMessages();
 	}
 	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public String addMessage() {
+		return "POST message";
+	}
+	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId")long messageId) {
 		return messageService.getMessage(messageId);
 	}
