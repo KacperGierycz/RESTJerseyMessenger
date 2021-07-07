6628213f24fa0f8fa65440bf43ddc2474cb87c5f
Kacper <kacpergierycz@gmail.com>
2021-07-07 12:16:42 +0200

    @PathParam in use getting a single Message from DB

--- messenger/src/main/java/rest/jersey/messenger/service/MessageService.java
+++ messenger/src/main/java/rest/jersey/messenger/service/MessageService.java
@@ -2,20 +2,50 @@ package rest.jersey.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import rest.jersey.messenger.database.DatabaseClass;
 import rest.jersey.messenger.model.Message;
 
 public class MessageService {
 
-	public List<Message> getAllMessages(){
+	private Map<Long,Message>messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1, "Hello World", "kacper"));
+		messages.put(2L, new Message(2, "Hello Jersey", "kacper"));
 		
-		Message m1=new Message(1L,"Hello World","kacper");
-		Message m2=new Message(2L,"Hello Jersey","kacper");
-		List<Message>messeges=new ArrayList();
-		messeges.add(m1);
-		messeges.add(m2);
-		return messeges;
-				
+	}
+	
+	public List<Message> getAllMessages(){
+	
+		return new ArrayList<Message>(messages.values());
+		
+	}
+	
+	public Message getMessage(long Id) {
+
+		return messages.get(Id);
+	}
+	
+	public Message addMessage(Message message) {
+		
+		message.setId(messages.size()+1);
+		messages.put(message.getId(),message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		
+		if (message.getId()==0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
 	}
 	
 }
