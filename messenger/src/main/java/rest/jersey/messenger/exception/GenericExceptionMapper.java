88ed77ec60f7736c3735b3c08a6b3392b1a273d5
Kacper <kacpergierycz@gmail.com>
2021-07-10 14:40:52 +0200

    common exception in java docs

--- messenger/src/main/java/rest/jersey/messenger/exception/GenericExceptionMapper.java
+++ messenger/src/main/java/rest/jersey/messenger/exception/GenericExceptionMapper.java
@@ -7,7 +7,7 @@ import javax.ws.rs.ext.Provider;
 
 import rest.jersey.messenger.model.ErrorMessage;
 
-@Provider
+
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 
 	@Override
